05c2e99fef2852fd6195b23207e73af875638de4
abishr <adhambishr@gmail.com>
2018-03-02 13:59:58 -0800

    filling out form

---
+++ client/src/components/add-job.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class AddJobs extends Component {
+
+    render() {
+        return (
+            <h1>Add Jobs Webpage
+            </h1>
+        )
+    }
+}
+
+export default AddJobs;
---
+++ client/src/components/jobs-display.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class JobsDisplay extends Component{
+
+    render(){
+        return(
+            <h1>Jobs Display Webpage </h1>
+        )
+    }
+}
+
+export default JobsDisplay;
