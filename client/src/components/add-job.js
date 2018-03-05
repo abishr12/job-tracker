af5d65fac70c739cdf068697dd7ce9a25300fd8c
abishr <adhambishr@gmail.com>
2018-03-04 17:29:18 -0800

    working dropdown

--- client/src/components/add-job.js
+++ client/src/components/add-job.js
@@ -1,61 +1,85 @@
-import React, {Component} from 'react';
-import {Field, reduxForm} from "redux-form";
-import {FormGroup, FormControl, ControlLabel} from 'react-bootstrap'
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { DropdownList } from "react-widgets";
+
 class AddJobs extends Component {
-    renderField(field) {
+  grabPositions() {
+    const positions = [
+      "Software Engineer",
+      "Full Stack Engineer",
+      "Front End Engineer",
+      "Back End Engineer",
+      "Intern",
+      "Other"
+    ];
+    return positions;
+  }
 
-        return (
-            <FormGroup>
-                <ControlLabel>{field.label}:
-                </ControlLabel>
-                <br/>
-                <FormControl type="text" {...field.input}/> 
-                {field.meta.error}
-            </FormGroup>
-        )
+  renderField(field) {
+    return (
+      <FormGroup>
+        <ControlLabel>{field.label}:</ControlLabel>
+        <br />
+        <FormControl type="text" {...field.input} /> {field.meta.error}
+      </FormGroup>
+    );
+  }
 
-    }
-    onSubmit(values){
-        console.log(values)
-    }
-    render() {
-        const {handleSubmit} = this.props
-        return (  
-            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-                < Field label="Company" name="company" component={this.renderField}/>
-                < Field label="Position" name="position" component={this.renderField}/>
-                < Field label="Link" name="link" component={this.renderField}/>
-                < button type="submit" className = "btn btn-primary">Submit</button>
-            </form>
+  renderDropdown(field) {
+    return (
+      <DropdownList
+        {...field.input}
+        data={field.data}
+        onChange={field.input.onChange}
+      />
+    );
+  }
+  onSubmit(values) {
+    console.log(values);
+  }
 
-        )
-    }
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field label="Company" name="company" component={this.renderField} />
+        <Field
+          label="Position"
+          name="position"
+          data={this.grabPositions()}
+          valueField="value"
+          textField="position"
+          component={this.renderDropdown}
+        />
+        <Field label="Link" name="link" component={this.renderField} />{" "}
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    );
+  }
 }
 
 function validate(values) {
+  const errors = {};
 
-    const errors = {}
+  // Validate Inputs
+  if (!values.company) {
+    errors.company = "Enter Company Name!";
+  }
 
-    // Validate Inputs
-    if (!values.company) {
-        errors.company = "Enter Company Name!";
+  if (!values.position) {
+    errors.position = "Enter Position Name!";
+  }
+  if (!values.link) {
+    errors.link = "Provide A Link";
+  }
 
-    }
-
-    if (!values.position) {
-        errors.position = "Enter Position Name!";
-
-    }
-    if (!values.link) {
-        errors.link = "Provide A Link";
-
-    }
-
-    return errors;
-
+  return errors;
 }
 export default reduxForm({
-    //validate: validate <--- the validate function above
-    validate, 
-    form: "AddJobsForm"
-})(AddJobs)
+  //validate: validate <--- the validate function above
+  validate,
+  form: "AddJobsForm"
+})(AddJobs);
--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from "react-redux";
-import {createStore, applyMiddleware} from "redux";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import reducers from "./reducers";
 import promise from "redux-promise";
+import "react-widgets/dist/css/react-widgets.css";
 
-import AddJobs from "./components/add-job"
-import JobDisplay from "./components/jobs-display"
+import AddJobs from "./components/add-job";
+import JobDisplay from "./components/jobs-display";
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-
+  <Provider store={createStoreWithMiddleware(reducers)}>
     <Router>
-        <Switch>
-            <Route exact path="/" component={JobDisplay}/>
-            < Route exact path="/add" component={AddJobs}/>
-        </Switch>
-
+      <Switch>
+        <Route exact path="/" component={JobDisplay} />
+        <Route exact path="/add" component={AddJobs} />
+      </Switch>
     </Router>
-
-</Provider>, document.querySelector("#root"));
+  </Provider>,
+  document.querySelector("#root")
+);
