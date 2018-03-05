5d5ea47921ae9f32d32727d36541ad077cb91c94
abishr <adhambishr@gmail.com>
2018-03-04 16:24:30 -0800

    valid visual form

--- client/src/components/add-job.js
+++ client/src/components/add-job.js
@@ -1,13 +1,59 @@
 import React, {Component} from 'react';
-
+import {Field, reduxForm} from "redux-form";
+import {FormGroup, FormControl, ControlLabel} from 'react-bootstrap'
 class AddJobs extends Component {
+    renderField(field) {
 
-    render() {
         return (
-            <h1>Add Jobs Webpage
-            </h1>
+            <FormGroup>
+                <ControlLabel>{field.label}:
+                </ControlLabel>
+                <br/>
+                <FormControl type="text" {...field.input}/> 
+                {field.meta.error}
+            </FormGroup>
+        )
+
+    }
+    render() {
+        const {handleSubmit} = this.props
+        return (
+            
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                < Field label="Company" name="company" component={this.renderField}/>
+                < Field label="Position" name="position" component={this.renderField}/>
+                < Field label="Link" name="link" component={this.renderField}/>
+                < button type="submit" className = "btn btn-primary">Submit</button>
+            </form>
+
         )
     }
 }
 
-export default AddJobs;
+function validate(values) {
+
+    const errors = {}
+
+    // Validate Inputs
+    if (!values.company) {
+        errors.company = "Enter Company Name!";
+
+    }
+
+    if (!values.position) {
+        errors.position = "Enter Position Name!";
+
+    }
+    if (!values.link) {
+        errors.link = "Provide A Link";
+
+    }
+
+    return errors;
+
+}
+export default reduxForm({
+    //validate: validate <--- the validate function above
+    validate, 
+    form: "AddJobsForm"
+})(AddJobs)
